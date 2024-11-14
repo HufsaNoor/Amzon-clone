85d889e293b4b5087ed15cd123702b995c98e9c3
Hufsa Noor <hufsanoor7975@gmail.com>
2024-11-14 06:33:13 +0500

    signin page

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -1,29 +1,3 @@
-// "use client"
-// import SearchResult from '@/components/shared/SearchResult'
-// import { useSupabase } from '@/lib/supabase/hooks/useSupabase'
-// import { useParams } from 'next/navigation'
-// import React, { useEffect } from 'react'
-
-// const Page = () => {
-//     const { query } = useParams();
-//     const { filterData, getFilteredData } = useSupabase();
-
-//     useEffect(() => {
-//         getFilteredData(query.toString());
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, []);
- 
-
-
-//     return (
-//         <div>
-//             <SearchResult filterData={filterData}/>
-//         </div>
-//     )
-// }
-
-// export default Page
-
 "use client"
 import SearchResult from '@/components/SearchResult'
 import { useSupabase } from '@/lib/supabase/hooks/useSupabase'
